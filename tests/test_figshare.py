62f55544e88f04d8c6701605577fa9d1b7776a91
Chun Ly <astro.chun@gmail.com>
2021-02-15 16:00:30 -0700

    Change testing article_id
     - Assert id metadata in test_figshare_get
     - Assert article_id metadata in test_metadata_get

--- tests/test_figshare.py
+++ tests/test_figshare.py
@@ -4,7 +4,7 @@ from readme_tool.figshare import app, api_version
 
 client = TestClient(app)
 
-article_id = 12735992
+article_id = 12966581
 stage_article_id = 6941778
 
 
@@ -13,11 +13,13 @@ def test_figshare_get():
     response = client.get(f'/api/{api_version}/figshare/{article_id}')
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
+    assert response.json()['id'] == article_id
 
     # Stage
     response = client.get(f'/api/{api_version}/figshare/{stage_article_id}?stage=True')
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
+    assert response.json()['id'] == stage_article_id
 
 
 def test_metadata_get():
@@ -25,8 +27,10 @@ def test_metadata_get():
     response = client.get(f'/api/{api_version}/metadata/{article_id}')
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
+    assert response.json()['article_id'] == article_id
 
     # Stage
     response = client.get(f'/api/{api_version}/metadata/{stage_article_id}?stage=True')
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
+    assert response.json()['article_id'] == stage_article_id
